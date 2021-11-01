b41a959f5b271bf60a31be817cf3c445e33cde8e
Alerrandro Borges <alerrandrokaton@gmail.com>
2021-11-01 09:25:48 -0300

    Urna eletronica

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,175 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="urna">
+
+      <Tela 
+        :tela="tela"
+        :numeroVoto="numeroVoto"
+        :quantidadeNumeros="quantidadeNumeros"
+        :canditato="canditato"
+      />
+
+      <Teclado 
+        :adicionarNumero="adicionarNumero"
+        :corrigi="corrigi"
+        :confirmar="confirmar"
+        :votaEmBranco="votaEmBranco"
+      />
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import '@/css/global.css';
+
+import Teclado from './components/Teclado';
+
+import Tela from './components/Tela';
+
+import confirmaAudio from '@/assets/audios/confirm.wav';
+
+import keyAudio from '@/assets/audios/key.wav';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Teclado,
+    Tela
+  },
+  methods:{
+    adicionarNumero(numero){
+      this.executaSom(keyAudio);
+
+      if(this.numeroVoto.length == this.quantidadeNumeros){
+        return false;
+        }
+        this.numeroVoto += ''+numero;
+
+        this.verificaCanditato();
+    },
+    verificaCanditato(){
+      if(this.numeroVoto.length < this.quantidadeNumeros){
+        return false;
+      }
+
+      if(this.canditatos[this.tela][this.numeroVoto]){
+        this.canditato = this.canditatos[this.tela][this.numeroVoto];
+        return true;
+      }
+
+      this.canditato = {
+        nome: 'Voto nulo',
+        partido: 'Voto nulo',
+        imagem: ''
+      }
+    },
+    corrigi(){
+      this.executaSom(keyAudio);
+      
+      this.limpar();
+    },
+    limpar(){
+      this.canditato = {};
+      this.numeroVoto = ''
+    },
+    confirmar(){
+       if(this.numeroVoto.length < this.quantidadeNumeros){
+        return false;
+      }
+
+      return this.avancaTela();
+    },
+    avancaTela(){
+      this.executaSom(confirmaAudio);
+
+      if(this.tela == 'prefeito'){
+        this.tela = 'vereador';
+        this.quantidadeNumeros = 5;
+        return this.limpar();
+      }
+
+      this.tela = 'fim';
+
+      var instancia = this
+
+      setTimeout(function(){
+        instancia.tela = 'prefeito';
+        instancia.quantidadeNumeros = 2;
+        return instancia.limpar();
+      },3000);
+    },
+    votaEmBranco(){
+      if(this.tela == 'fim') return false;
+      
+      this.limpar();
+      this.avancaTela();
+    },
+    executaSom(arquivoSom){
+      if(arquivoSom){
+        var audio = new Audio(arquivoSom);
+        audio.play();
+      }
+    }
+  },
+  data(){
+    return{
+      tela:'prefeito',
+      numeroVoto: '',
+      quantidadeNumeros: 2,
+      canditato: {},
+      canditatos:{
+        "prefeito":{
+          "01":{
+            "nome": "Ash",
+            "partido": "Pokemon",
+            "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/ash.png"
+          },
+          "08":{
+            "nome": "Vegeta",
+            "partido": "Dragon Ball",
+            "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/vegeta.png"
+          }
+        },
+        "vereador":{
+          "01234":{
+            "nome": "Pikachu",
+            "partido": "Pokemon",
+            "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/pikachu.png"
+          },
+          "08001":{
+            "nome": "Goku",
+            "partido": "Dragon Ball",
+            "imagem": "https://raw.githubusercontent.com/william-costa/wdev-urna-eletronica-resources/master/images/goku.png"
+          }
+        }
+      }
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-color: var(--background-color);
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.urna{
+  width: 1000px;
+  height: 500px;
+  background-color: var(--ballot-box-background-color);
+  padding: 30px;
+  border-radius: 5px;
+  display: flex;
+  justify-content: space-between;
+  
 }
 </style>
